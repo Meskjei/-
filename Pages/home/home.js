2231220b640b515c4eb37b19633d1db7028ba3e0
Zheng_Wenkai <841072130@qq.com>
2018-10-24 23:23:26 +0800

    no message

--- Pages/home/home.js
+++ Pages/home/home.js
@@ -1,10 +1,14 @@
 // Pages/home/home.js
+const utils = require('../../utils/utils');
+const db_utils = require('../../utils/db_utils');
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    news:null,
     activityType: ['全部', '数学与信息学院', '软件学院', '材料与能源学院', '工程学院', '国际教育学院', '经济管理学院', '林学与风景园林学院', '园艺学院', '资源环境学院', '电子工程学院', '动物科学学院', '海洋学院', '兽医学院', '水利与土木工程学院', '人文与法学学院', '公共管理学院', '外国语学院', '食品学院', '艺术学院', '农学院', '生命科学学院'],
     currentActivityIndex: 0,
     popularActivity: [{ picUrl:'https://ss3.baidu.com/9fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=06f18776399b033b3388fada25cf3620/77c6a7efce1b9d162f210013fedeb48f8d5464da.jpg', title: 'Test'}]
@@ -20,55 +24,12 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    db_utils.searchData(app.globalData.newsTableId,undefined,(res)=>{
+      console.log(res)
+      this.setData({
+        news:res
+      })
+      console.log(this.data.news);
+    })
   }
 })
