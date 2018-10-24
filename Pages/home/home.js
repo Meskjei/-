b8da799d2aa6747f16a47ada917718ce55fde926
unknown <unknown@example.com>
2018-10-24 19:35:27 +0800

    Initial Commit

--- Pages/home/home.js
+++ Pages/home/home.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    activityType: ['全部', '数学与信息学院', '软件学院', '材料与能源学院', '工程学院', '国际教育学院', '经济管理学院', '林学与风景园林学院', '园艺学院', '资源环境学院', '电子工程学院', '动物科学学院', '海洋学院', '兽医学院', '水利与土木工程学院', '人文与法学学院', '公共管理学院', '外国语学院', '食品学院', '艺术学院', '农学院', '生命科学学院'],
+    activityType: ['全部', '数信学院', '工程学院', '农学院', '食品学院', '兽医学院'],
     currentActivityIndex: 0,
     popularActivity: [{ picUrl:'https://ss3.baidu.com/9fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=06f18776399b033b3388fada25cf3620/77c6a7efce1b9d162f210013fedeb48f8d5464da.jpg', title: 'Test'}]
   },
--- app.js
+++ app.js
@@ -4,11 +4,7 @@ App({
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function () {
-    let that = this
-    // 引入 BaaS SDK
-    require('./utils/sdk-v1.9.0')
-    let clientId = this.globalData.clientId
-    wx.BaaS.init(clientId)
+    
   },
 
   /**
@@ -30,9 +26,5 @@ App({
    */
   onError: function (msg) {
     
-  },
-  globalData: {
-    clientId: '46408eb85ce79773f291',
-    tableId: 55328,
   }
 })
