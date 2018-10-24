15987713ff7cb36c5f0a29f8644f6f7caffdcb69
Zheng_Wenkai <841072130@qq.com>
2018-10-24 21:28:17 +0800

    add login

--- Pages/login/login.js
+++ Pages/login/login.js
@@ -1,11 +1,14 @@
-// Pages/login/login.js
+const util = require('../../utils/utils');
+const db_util = require('../../utils/db_utils');
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    password: null,
+    username: null
   },
 
   /**
@@ -15,52 +18,26 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getUsername: function (e) {
+    this.setData({
+      username: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  getPassword: function (e) {
+    this.setData({
+      password: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  loginClick: function (e) {
+    app.globalData.userInfo = { username: this.data.username, password: this.data.password }
+    if (app.globalData.userInfo.username == 'interviewer' && app.globalData.userInfo.password == 'scau2018!') {
+      util.showSuccess('登录成功');
+      wx.redirectTo({
+        url: '../home/home',
+      })
+    }
+    else {
+      util.showModel('登录失败', '账户或密码错误');
+    }
   }
 })
